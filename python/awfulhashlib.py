1c9c9328d3a765d98965e45d4edd4a50854b6789
Michael Hurwitz <dangermike@gmail.com>
2022-01-02 20:20:28 -0500

    awfulhashlib improvements
    * Using buffer protocol stuff for high-performance terrible hashes
    * Matching the Python 3 hashlib interface
    * Fallback to hashlib.new to allow awfulhashlib to stand-in for hashlib to meet all your terrible hashing needs

--- python/awfulhashlib.py
+++ python/awfulhashlib.py
@@ -1,85 +1,156 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 import binascii
+import hashlib
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from _typeshed import ReadableBuffer
 
 
 class AwfulHash64(object):
-    def __init__(self, data=None):
+
+    def __init__(self, data: ReadableBuffer = None):
         self._hashval = 0b1111111111111101100110011010000110010111100000101100100011001001
-        self._remnant = bytes(0)
+        self._remnant = bytearray(0)
         if data:
             self.update(data)
 
     @property
-    def name():
+    def digest_size(self) -> int:
+        return 8
+
+    @property
+    def block_size(self) -> int:
+        return 8
+
+    @property
+    def name(self) -> str:
         return 'AwfulHash64'
 
-    def update(self, data):
-        while len(data) >= 8:
-            new_bytes = self._remnant + data[0:8 - len(self._remnant)]
-            self._hashval ^= int.from_bytes(new_bytes, byteorder='big')
-            data = data[8:]
+    def _remnant_val(self) -> int:
+        return int.from_bytes(
+            self._remnant, byteorder='big') << (64 - (len(self._remnant) * 8))
 
-        self._remnant = data
-        return self
+    def copy(self) -> AwfulHash64:
+        h = AwfulHash64()
+        h._hashval = self._hashval
+        h._remnant = self._remnant.copy()
 
-    def digest(self):
+        return h
+
+    def digest(self) -> bytes:
         final_val = \
-            self._hashval ^ \
-            int.from_bytes(self._remnant + bytes(8-len(self._remnant)), byteorder='big')
+            self._hashval ^ self._remnant_val()
         return final_val.to_bytes(8, byteorder='big')
 
-    def hexdigest(self):
+    def hexdigest(self) -> str:
         return binascii.hexlify(self.digest()).decode('utf-8')
+
+    def update(self, data: ReadableBuffer):
+        mv = memoryview(data)
+        nb = mv.nbytes
+        start = 0
+        while start + 8 < nb:
+            if len(self._remnant) > 0:
+                # read the remnant
+                val = self._remnant_val()
+                to_read = 8 - len(self._remnant)
+                val += int.from_bytes(mv[start:start + to_read],
+                                      byteorder='big')
+                start += to_read
+                self._remnant.clear()
+            else:
+                val = int.from_bytes(mv[start:start + 8], byteorder='big')
+                start += 8
+
+            self._hashval ^= val
+
+        if start < nb:
+            self._remnant[0:nb - start] = mv[start:]
 
 
 class BarfHash64(object):
-    def __init__(self, data=None):
+
+    def __init__(self, data: ReadableBuffer = None):
         self._hashval = 0b1111111111111101100110011010000110010111100000101100100011001001
-        self._remnant = bytes(0)
+        self._remnant = bytearray(0)
         self._blocks = 0
         if data:
             self.update(data)
 
     @property
-    def name():
+    def digest_size(self) -> int:
+        return 8
+
+    @property
+    def block_size(self) -> int:
+        return 8
+
+    @property
+    def name(self) -> str:
         return 'BarfHash64'
 
+    def _remnant_val(self) -> int:
+        return int.from_bytes(
+            self._remnant, byteorder='big') << (64 - (len(self._remnant) * 8))
+
+    def copy(self) -> BarfHash64:
+        h = BarfHash64()
+        h._hashval = self._hashval
+        h._remnant = self._remnant.copy()
+        h._blocks = self._blocks
+
+        return h
+
+    def digest(self) -> bytes:
+        final_val = \
+            self._hashval ^ self._remnant_val()
+        return final_val.to_bytes(8, byteorder='big')
+
+    def hexdigest(self) -> str:
+        return binascii.hexlify(self.digest()).decode('utf-8')
+
+    def update(self, data: ReadableBuffer):
+        mv = memoryview(data)
+        nb = mv.nbytes
+        start = 0
+        while start + 8 < nb:
+            if len(self._remnant) > 0:
+                # read the remnant
+                val = self._remnant_val()
+                to_read = 8 - len(self._remnant)
+                val += int.from_bytes(mv[start:start + to_read],
+                                      byteorder='big')
+                start += to_read
+                self._remnant.clear()
+            else:
+                val = int.from_bytes(mv[start:start + 8], byteorder='big')
+                start += 8
+
+            self._blocks += 1
+            self._hashval ^= val
+            self._hashval = BarfHash64._shift(self._hashval, self._blocks)
+
+        if start < nb:
+            self._remnant[0:nb - start] = mv[start:]
+
     @staticmethod
-    def _shift(base, n):
+    def _shift(base: int, n: int) -> int:
         shift = (11 * n) % 64
         return ((base << shift) & 0xFFFFFFFFFFFFFFFF) | (base >> (64 - shift))
 
-    def update(self, data):
-        while len(data) >= 8:
-            self._blocks += 1
-            new_bytes = self._remnant + data[0:8 - len(self._remnant)]
-            self._hashval ^= int.from_bytes(new_bytes, byteorder='big')
-            self._hashval = BarfHash64._shift(self._hashval, self._blocks)
-            data = data[8:]
-
-        self._remnant = data
-        return self
-
-    def digest(self):
-        final_val = BarfHash64._shift(
-            self._hashval ^ int.from_bytes(self._remnant + bytes(8-len(self._remnant)), byteorder='big'),
-            self._blocks + 1
-        )
-        return final_val.to_bytes(8, byteorder='big')
-
-    def hexdigest(self):
-        return binascii.hexlify(self.digest()).decode('utf-8')
-
 
 class AwfulHashLib(object):
-    def __init__():
+
+    def __init__(self):
         pass
 
     @staticmethod
-    def new(name, data=None):
+    def new(name: str, data: ReadableBuffer = b'', **kwargs):
         name = name.lower()
         if name == 'awfulhash64':
-            h = AwfulHash64()
-            if data:
-                h.update(data)
-            return h
+            return AwfulHash64(data)
+        if name == 'barfhash64':
+            return BarfHash64(data)
+        return hashlib.new(name, data, **kwargs)
